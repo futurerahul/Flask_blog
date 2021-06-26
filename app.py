79984e2818cdf23d7354c2dddbf0edaba3bcc30f
rahul chauhan <rah.chauhan1@gmail.com>
2021-06-27 02:11:01 +0530

    added form and signup page template

--- app.py
+++ app.py
@@ -1,11 +1,26 @@
 from flask import Flask, render_template
+from forms import SignupForm, LoginForm
 
 app = Flask(__name__)
+
+app.config["SECRET_KEY"] = "fSXfzQg6QrS0SNqRcMC1"
 
 
 @app.route("/")
 def index():
     return render_template("home.html")
+
+
+@app.route("/signup")
+def signup():
+    form = SignupForm()
+    return render_template("signup.html", title="Signup", form=form)
+
+
+@app.route("/login")
+def login():
+    form = SignupForm()
+    return render_template("login.html", title="Login", form=form)
 
 
 if __name__ == "__main__":
